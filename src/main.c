95f7682c0f231ab78142b742d0a37cd250f265a3
Guilherme <guilhermebispo512@gmail.com>
2025-08-19 08:59:06 -0300

    fix double buffer bug

--- src/main.c
+++ src/main.c
@@ -85,7 +85,7 @@ int main(int argc, char **argv)
 
 
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB); 
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); 
     glutInitWindowSize(200, 200);
     glutInitWindowPosition(100, 100);          
     glutCreateWindow("Paint 2025 atualizado Premium");
