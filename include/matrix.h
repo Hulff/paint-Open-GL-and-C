f30c5831808cdd9e8261018f136b48a423c602c6
Hugo <ph2717693@gmail.com>
2025-08-19 21:55:23 -0300

    add matrix multiplication function prototype missing implementation

---
+++ include/matrix.h
@@ -0,0 +1,6 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+void multiplicar_matrizes(float **matriz1, float **matriz2, float **resultado, int linhas1, int colunas1, int colunas2);
+
+#endif
---
+++ src/utils/matrix.c
@@ -0,0 +1,11 @@
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+void multiplicar_matrizes(float **matriz1, float **matriz2, float **resultado, int linhas, int colunas1, int colunas2)
+{
+    
+}
